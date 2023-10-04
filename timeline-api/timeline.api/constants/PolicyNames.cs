8dbfb176800fe88a934cfda3212fa0b0f49bb574
tseringggg <47801677+tseringgg@users.noreply.github.com>
2023-10-04 14:11:14 -0700

    Delete old projects and fixes

--- timeline-api/timeline.api/constants/PolicyNames.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace timeline.api.constants
-{
-    public static class PolicyNames
-    {
-        public const string Administrator = "timeline_admin";
-        public const string ReadOnlyUser = "timeline_readonly_user";
-        public const string ReadWriteUser = "timeline_readwrite_user";
-    }
-}
